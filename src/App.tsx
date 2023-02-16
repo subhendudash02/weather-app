fd6eb63aacfdfa1886710edcc51b56c405e2a471
Subhendu Dash <sdash29102@gmail.com>
2023-02-16 16:56:27 +0530

    add current weather details

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import {MutableRefObject, useRef, useState} from 'react';
+import url from "./utils/weatherapi";
+
+import "./styles/App.css";
+
+interface WeatherData {
+    name: string;
+    temp_celsius: number;
+    temp_fahrenheit: number;
+    humidity: number;
+    wind_kph: number;
+    comment: string;
+}
 
 function App() {
+    const getData = useRef() as MutableRefObject<HTMLInputElement>;
+    const [weather, getWeatherData] = useState<WeatherData>({
+        name: "",
+        temp_celsius: 0,
+        temp_fahrenheit: 0,
+        humidity: 0,
+        wind_kph: 0,
+        comment: "",
+    });
+
+    const getWeather = async (e:any) => {
+        e.preventDefault();
+
+        // Current Weather Info
+        await url.get<any>(`/current.json?key=7f75fdc3787c48a29a574714231602&q=${getData.current.value}&aqi=no`)
+            .then(res => {
+                getWeatherData({
+                    name: res.data.location.name,
+                    temp_celsius: res.data.current.temp_c,
+                    temp_fahrenheit: res.data.current.temp_f,
+                    humidity: res.data.current.humidity,
+                    wind_kph: res.data.current.wind_kph,
+                    comment: res.data.current.condition.text
+                })
+            });
+    }
+
   return (
     <div className="App">
-        <h1>Weather App</h1>
+        <h1 id="main-heading">Weather App</h1>
+
+        <form onSubmit={getWeather}>
+            <input type="text" name="city" placeholder="Enter a city" ref={getData} />
+        </form>
+        
+        {weather.name ? <p className="weather-data name">{weather.name}</p> : null}
+        {weather.temp_celsius ? <p className="weather-data temp">{weather.temp_celsius}°C</p> : null}
+        {weather.comment ? <p className="weather-data comment">{weather.comment}</p> : null}
+        {weather.humidity && weather.wind_kph ? <p className="weather-data humidity">Humidity: {weather.humidity}% | Wind: {weather.wind_kph} kph</p> : null}
     </div>
   );
 }
